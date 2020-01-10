fb8e50d00101759923d161f5a1db6e6d46ce7c08
Ahmed <hello@clivern.com>
2020-01-10 15:39:51 +0100

    Update store.py

--- store.py
+++ store.py
@@ -2,3 +2,6 @@ from postgres import Postgres
 from random import randint
 
 db = Postgres("postgres://postgres:password@x.x.x.x:5432/tutorial")
+
+for i in range(100000000000):
+    print(db.run("INSERT INTO conditions(time, location, temperature, humidity) VALUES (NOW(), 'office', " + str(float(randint(0, 100))) + ", " + str(float(randint(0, 100))) + ")"))
